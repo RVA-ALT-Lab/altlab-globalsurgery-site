eb49c7c60edf38da014ac6ad34158cf69030966a
heyohbunkbunk <mdroberts@vcu.edu>
2020-05-18 15:40:56 -0400

    Updating the main plugin
    Now doing Quiz Check, creating the units bar, hiding elements if not logged in, Registration bypass fix, GF fields read only for name and email.

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: ALT Lab Global Surgery custom site stuff
 Plugin URI:  https://github.com/
-Description: Making the Global Surgery site better
-Version:     1.82
+Description: Global Surgery Registation fixes (JS), Read-only GF Registration fields (JS), Quiz Completion Checker, Units Bar Creator, etc.
+Version:     1.85
 Author:      ALT Lab (Matt Roberts)
 Author URI:  http://altlab.vcu.edu
 License:     GPL2
@@ -19,7 +19,7 @@ add_action('wp_enqueue_scripts', 'global_surg_load_scripts');
 
 function global_surg_load_scripts() {                           
     $deps = array('jquery');
-    $version= '1.82'; 
+    $version= '1.85'; 
     $in_footer = true;    
     wp_enqueue_script('global-surg-main-js', plugin_dir_url( __FILE__) . 'js/global-surg-main.js', $deps, $version, $in_footer); 
     wp_enqueue_style( 'global-surg-main-css', plugin_dir_url( __FILE__) . 'css/global-surg-main.css');
@@ -31,6 +31,7 @@ add_filter( 'gform_user_registration_check_email_pre_signup_activation', '__retu
 add_filter( 'gform_user_registration_validation', 'ignore_already_registered_error', 10, 3 );
 
 add_action("gform_user_registration_validation", "ignore_already_registered_error", 10, 3);
+
 function ignore_already_registered_error($form, $config, $pagenum){
  
     // Make sure we only run this code on the specified form ID
@@ -53,7 +54,82 @@ function ignore_already_registered_error($form, $config, $pagenum){
  
 }
 
-
+// Quiz Competion Checker
+function quiz_spitter() {
+    $form_id = 0; //Look through all quizzes (can be more targeted)
+    $current_user = wp_get_current_user(); //Gets current logged in user
+    $search_criteria = array(
+        //Looking for quizzes passed by the current logged in user
+        'field_filters' => array(
+           'mode' => 'all',
+           array(
+                 'key'   => 'gquiz_is_pass',
+                 'value' => '1'
+           ),
+           array(
+              'key' => 'created_by',
+              'value' => $current_user->ID
+           ),
+        )
+     );
+    $entries = GFAPI::get_entries($form_id, $search_criteria, $sorting, $paging, $total_count);
+  
+    $form_ids = array_column($entries, 'form_id');
+  
+    echo '<div id="units-bar"><ul class="unit-list">';
+  
+    if (in_array('1', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-1">&#x2713; Unit 1</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-1">Unit 1</li>';
+        }
+    if (in_array('2', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-2">&#x2713; Unit 2</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-2">Unit 2</li>';
+        }
+    if (in_array('3', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-3">&#x2713; Unit 3</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-3">Unit 3</li>';
+        }
+    if (in_array('4', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-4">&#x2713; Unit 4</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-4">Unit 4</li>';
+        }
+    if (in_array('5', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-5">&#x2713; Unit 5</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-5">Unit 5</li>';
+        }
+    if (in_array('6', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-6">&#x2713; Unit 6</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-6">Unit 6</li>';
+        }
+    if (in_array('7', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-7">&#x2713; Unit 7</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-7">Unit 7</li>';
+        }
+    if (in_array('8', $form_ids)) {
+     echo '<li class="unit-item-done" id="unit-8">&#x2713; Unit 8</li>';
+     } else {
+        echo '<li class="unit-item" id="unit-8">Unit 8</li>';
+        }
+  
+    echo '</ul><div id="complete-mess"><p>&#x2713; = passed unit quiz</p></div></div>';
+  
+  //   print("<pre>".print_r($form_ids, true)."</pre>");
+  
+  //   var_dump($form_id);
+  //   print("<pre>".print_r($entries, true)."</pre>");
+  
+  }
+  
+  add_shortcode( 'spit', 'quiz_spitter' );
+  
 //LOGGER -- like frogger but more useful
 
 if ( ! function_exists('write_log')) {
--- js/global-surg-main.js
+++ js/global-surg-main.js
@@ -1,10 +1,14 @@
 //Check if logged in user and do stuff
+//Make fields read-only
 
-window.addEventListener('DOMContentLoaded', checkLoggedInUser);
+window.addEventListener('DOMContentLoaded', () => {
+    checkLoggedInUser();
+    makeReadOnlyFields();
+});
 
 function checkLoggedInUser () {
     if(document.body.classList.contains( 'logged-in' )) {
-        console.log("yep, logged in");
+        console.log("yep, you're logged in");
         if(document.getElementById("register")) {
             document.getElementById("register").className = "register-hide";
         }
@@ -14,6 +18,24 @@ function checkLoggedInUser () {
         if(document.getElementById("register-message")) {
             document.getElementById("register-message").className = "register-hide";
         }
+        if(document.getElementById("site-header")) {
+            if(document.getElementById("units-bar")) {
+                document.getElementById("site-header")
+                .appendChild(document.getElementById("units-bar"));
+            }
+        }
      } else {
-          /* redirect */}
+        if(document.getElementById("units-bar")) {
+            document.getElementById("units-bar").className = "login-hide";
+        }
+    }
+}
+
+function makeReadOnlyFields () {
+    if (document.querySelector('.gform_footer input[type="submit"]')) {
+        jQuery(document).ready(function(){
+            /* apply only to a input with a class of gf_readonly */
+            jQuery("li.gf_readonly input").attr("readonly","readonly");
+        });
+    }
 }
