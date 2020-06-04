219b7bee4d158219eb7e638d9e38cd6d80af3540
heyohbunkbunk <mdroberts@vcu.edu>
2020-06-04 10:06:24 -0400

    Turned off disable email check on registration
    Why did I have this on?

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: ALT Lab Global Surgery custom site stuff
 Plugin URI:  https://github.com/
 Description: Global Surgery Registation fixes (JS), Read-only GF Registration fields (JS), Quiz Completion Checker, Units Bar Creator, etc.
-Version:     1.85
+Version:     1.86
 Author:      ALT Lab (Matt Roberts)
 Author URI:  http://altlab.vcu.edu
 License:     GPL2
@@ -19,40 +19,40 @@ add_action('wp_enqueue_scripts', 'global_surg_load_scripts');
 
 function global_surg_load_scripts() {                           
     $deps = array('jquery');
-    $version= '1.85'; 
+    $version= '1.86'; 
     $in_footer = true;    
     wp_enqueue_script('global-surg-main-js', plugin_dir_url( __FILE__) . 'js/global-surg-main.js', $deps, $version, $in_footer); 
     wp_enqueue_style( 'global-surg-main-css', plugin_dir_url( __FILE__) . 'css/global-surg-main.css');
 }
 
 //Disable the check for an email already being used by a registered user
-add_filter( 'gform_user_registration_check_email_pre_signup_activation', '__return_false' );
+// add_filter( 'gform_user_registration_check_email_pre_signup_activation', '__return_false' );
 
-add_filter( 'gform_user_registration_validation', 'ignore_already_registered_error', 10, 3 );
+// add_filter( 'gform_user_registration_validation', 'ignore_already_registered_error', 10, 3 );
 
-add_action("gform_user_registration_validation", "ignore_already_registered_error", 10, 3);
+// add_action("gform_user_registration_validation", "ignore_already_registered_error", 10, 3);
 
-function ignore_already_registered_error($form, $config, $pagenum){
+// function ignore_already_registered_error($form, $config, $pagenum){
  
-    // Make sure we only run this code on the specified form ID
-    if($form['id'] != 10) {
-        return $form;
-    }
+//     // Make sure we only run this code on the specified form ID
+//     if($form['id'] != 10) {
+//         return $form;
+//     }
  
-    // Get the ID of the email field from the User Registration config
-    $email_id = $config['meta']['email'];
+//     // Get the ID of the email field from the User Registration config
+//     $email_id = $config['meta']['email'];
  
-    // Loop through the current form fields
-    foreach($form['fields'] as &$field) {
+//     // Loop through the current form fields
+//     foreach($form['fields'] as &$field) {
  
-    // confirm that we are on the current field ID and that it has failed validation because the email already exists
-    if($field->id == $email_id && $field->validation_message == 'Sorry, that email address is already used!')
-        $field->failed_validation = false;
-    }
+//     // confirm that we are on the current field ID and that it has failed validation because the email already exists
+//     if($field->id == $email_id && $field->validation_message == 'Sorry, that email address is already used!')
+//         $field->failed_validation = false;
+//     }
  
-    return $form;
+//     return $form;
  
-}
+// }
 
 // Quiz Competion Checker
 function quiz_spitter() {
@@ -107,7 +107,7 @@ function quiz_spitter() {
      echo '<button class="units-bar-button-done"><li class="unit-item-done" id="unit-6">&#x2713; Unit 6</li></button>';
      } else {
         echo '<a href="https://rampages.us/vcuglobalsurgery/unit-6-ethics/"><button class="units-bar-button"><li class="unit-item" id="unit-6">Unit 6</li></button></a>';
-        }
+        }in
     if (in_array('8', $form_ids)) {
      echo '<button class="units-bar-button-done"><li class="unit-item-done" id="unit-7">&#x2713; Unit 7</li></button>';
      } else {
